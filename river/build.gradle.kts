215bb21031a34ea99cebb57ac658da62c8ad7161
Felix Rabe <frab3@users.noreply.github.com>
2023-03-11 08:04:48 +0100

    Fix missing gradle-wrapper.jar (and bump versions)

--- river/build.gradle.kts
+++ river/build.gradle.kts
@@ -3,7 +3,7 @@
  * @author Fred George  fredgeorge@acm.org
  * Licensed under the MIT License; see LICENSE file in root.
  */
- 
+
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     kotlin("jvm")
@@ -20,10 +20,10 @@ repositories {
 
 dependencies {
     // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.10"))
 
     // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
 
     // Jupiter using JUnit 5
     testImplementation(platform("org.junit:junit-bom:5.9.2"))
